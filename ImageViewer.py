29dac9538b4d789398643ff7303ada8badccdee0
Pietro Zarri <pietrozarri96@gmail.com>
2021-04-16 18:05:44 +0200

    add sidebar to show image data

--- ImageViewer.py
+++ ImageViewer.py
@@ -1,5 +1,6 @@
 import PyQt5
 from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt, QSize
 from PyQt5.QtGui import QImage, QPixmap, QPalette
 
 from mainwindow import Ui_MainWindow
@@ -11,23 +12,45 @@ class ImageViewer (QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.pixmap = None
+        self.filename = None
+        self.info_box_is_visible = False
+        self.ui.info_box.setVisible(self.info_box_is_visible)
         self.ui.left_rotate.clicked.connect(self.left_rotate_button_clicked)
         self.ui.load_image.clicked.connect(self.open)
+        self.ui.show_info.clicked.connect(self.show_info_box)
         self.ui.image_box.setStyleSheet('background-color: cyan')
 
-        #self.ui.splitter.setStretchFactor(0, 0)
-        #self.ui.splitter.setStretchFactor(0, 1)
+
 
     def left_rotate_button_clicked(self):
         print("Left rotation")
 
+    def show_info_box(self):
+        print("enable")
+        self.info_box_is_visible = not self.info_box_is_visible
+        self.ui.info_box.setVisible(self.info_box_is_visible)
+        print(self.info_box_is_visible)
+
+    def resizeEvent(self, a0: QtGui.QResizeEvent):
+        self.update_view()
+
+    def update_view(self):
+        if self.pixmap is not None:
+            self.pixmap = QPixmap(self.filename)
+            width = max(500, self.ui.image_box.width())
+            height = max(300, self.ui.image_box.height())
+            self.ui.image_box.setPixmap(
+                self.pixmap.scaled(QSize(width, height), Qt.KeepAspectRatio, Qt.SmoothTransformation))
 
     def open(self):
         options = QFileDialog.Options()
         # fileName = QFileDialog.getOpenFileName(self, "Open File", QDir.currentPath())
-        fileName, _ = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
+        self.filename, _ = QFileDialog.getOpenFileName(self, 'QFileDialog.getOpenFileName()', '',
                                                   'Images (*.png *.jpeg *.jpg *.bmp *.gif)', options=options)
-        pixmap = QtGui.QPixmap(fileName)
-        height_label = 100
-        #self.ui.image_box.resize(self.ui.image_box.width(), self.ui.image_box.width())
-        self.ui.image_box.setPixmap(pixmap.scaled(self.ui.image_box.size()))
+        self.pixmap = QPixmap(self.filename)
+        width = self.pixmap.width()
+        height = self.pixmap.height()
+        self.ui.image_box.setPixmap(
+            self.pixmap.scaled(QSize(min(width, 1600), min(900, height)), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        self.update_view()
